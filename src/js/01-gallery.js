a238e6ab2f2ad9a85de8fb599645f19a03c7d1ae
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-07-31 18:31:16 +0300

    hw2,3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => `
+<li class="gallery__item" style="list-style-type: none">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>
+`).join('');
+
+
+gallery.innerHTML = galleryMarkup;
+
+
+new SimpleLightbox('.gallery a');
